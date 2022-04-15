32aec9deb3e8592758d084e369e2df27530c2b6e
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-14 21:29:02 -0700

    function that returns the conjugate of a number

---
+++ 0x00-math_complex/1-conjugate.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * display_complex_number - displays complex numbers
+ * @c: an struct which contains the complex field of a number
+ * Return: void function
+ */
+complex conjugate(complex c)
+{
+	c.re *= c.re;
+	c.im *= -1;
+	return (c);
+}
