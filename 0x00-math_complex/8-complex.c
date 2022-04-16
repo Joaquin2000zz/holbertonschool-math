d6451634e3f3ebb919d6b037794148e226fb6332
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-16 07:07:38 -0700

    function that update the real and the imaginary parts of a complex number given its modulus and arguments using SOH to the imaginary part and CAH to the real part

--- 0x00-math_complex/8-complex.c
+++ 0x00-math_complex/8-complex.c
@@ -2,12 +2,12 @@
 
 /**
  * complex_from_mod_arg - obtain a complex with modulus and arg
- * @c1: the first complex number
- * @c2: the second complex number
- * @c3: the pointer to storage the division
+ * @m: the modulus og the complex number (hypotenuse)
+ * @arg: the argument of the complex number (alpha)
+ * @c3: the pointer to storage the complex
  * Return: void function
  */
-void complex_from_mod_arg (double m, double arg, complex *c3)
+void complex_from_mod_arg(double m, double arg, complex *c3)
 {
 	(*c3).re = cos(arg) * m, c3->im = sin(arg) * m;
 }
--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -14,8 +14,8 @@
  */
 typedef struct complex
 {
-	int re;
-	int im;
+	double re;
+	double im;
 } complex;
 
 void display_complex_number(complex c);
@@ -26,5 +26,6 @@ void addition(complex c1, complex c2, complex *c3);
 void substraction(complex c1, complex c2, complex *c3);
 void multiplication(complex c1, complex c2, complex *c3);
 void division(complex c1, complex c2, complex *c3);
+void complex_from_mod_arg (double m, double arg, complex *c3);
 
 #endif
