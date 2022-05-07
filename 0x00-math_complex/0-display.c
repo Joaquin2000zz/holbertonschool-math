1376347682c6b6b30d9a00ceb3f572841055db44
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-06 19:04:28 -0700

    handled optional decimal precision

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,15 +6,15 @@
  */
 void display_complex_number(complex c)
 {
-	printf("%d", c.re);
+	printf("%g", c.re);
 	if (c.im > 0)
 		printf(" + ");
 	if (c.im < 0)
 		printf(" - ");
-	if (c.im > 1)
-		printf("%di", c.im);
+	if (c.im > 0.0 && c.im != 1)
+		printf("%gi", c.im);
 	if (c.im < -1)
-		printf("%di", c.im * -1);
+		printf("%gi", c.im * -1);
 	if (c.im == 1 || c.im == -1)
 		printf("i");
 	printf("\n");
